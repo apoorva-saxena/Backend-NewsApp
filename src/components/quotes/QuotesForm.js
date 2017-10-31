996a27db7990cb44f63e8519c7ae15e7c2d5f68c
Apoorva Saxena <apsaxena24@gmail.com>
2017-10-31 22:18:39 +0000

    More Quotes

--- src/components/quotes/QuotesForm.js
+++ src/components/quotes/QuotesForm.js
@@ -2,21 +2,32 @@ import React, {Component} from 'react'
 import FormGroup from 'react-bootstrap/lib/FormGroup'
 import Textarea from 'react-textarea-autosize'
 import FormControl from 'react-bootstrap/lib/FormControl'
+import LaddaButton from 'react-ladda'
+import 'ladda/dist/ladda-themeless.min.css'
+import './QuotesForm.css'
 
 class QuotesEdit extends Component {
+  constructor() {
+    super()
+  }
+
+  handleSubmit = (event) => {
+    console.log("I was called")
+    alert('event: ', event)
+  }
+
+  handleChangeValue = (e) => {
+
+  }
+
+  handleChangeSource = (e) => {
+
+  }
+
   render() {
     return (
-      <div>
-        <FormGroup>
-          <Textarea minRows={3} maxRows={10} defaultValue="Enter Quote..."></Textarea>
-          <FormControl
-            type="text"
-            // value={this.state.value}
-            placeholder="Enter Source"
-            // onChange={this.handleChange}
-          />
-
-        </FormGroup>
+     <div>
+      
       </div>
     )
   }
