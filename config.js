2bc64d2e67013d2e4958f9c4a3c465cfa7aafec8
Apoorva Saxena <apsaxena24@gmail.com>
2017-11-25 19:08:53 +0000

    Updated config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-
 const config = {
-key : process.env.APP_API_KEY,
-sessionKey : process.env.SESSION_KEY
+  key: process.env.APP_API_KEY,
+  sessionKey: process.env.SESSION_KEY
 }
 
 module.exports = config
